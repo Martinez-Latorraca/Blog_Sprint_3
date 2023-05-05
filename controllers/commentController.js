e1548a5c61e9f921707661fb212a050820f0e7fa
estebanCastaneira <estebanc_b@hotmail.com>
2023-05-05 16:17:55 -0300

    commentController

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,7 +2,8 @@ const { Article, Comment } = require("../models");
 
 // Display the specified resource.
 async function show(req, res) {
-  const comments = await Comment.findAll({ include: "article" });
+  const comments = await Comment.findAll({where: { articleId: req.body.id}},{ include: "article" });
+  console.log(comments)
   res.render("article", {
     comments,
   });
@@ -12,9 +13,11 @@ async function show(req, res) {
 async function store(req, res) {
   const comments = await Comment.create({
       fullname: req.body.fullname,
-      comment: req.body.comment,
-      articleId: res.params.id
+      content: req.body.content,
+      articleId: req.body.id
   });
+  console.log(res);
+  res.redirect(201, `/articulos/${comments.articleId}`)
 }
 
 module.exports = {
