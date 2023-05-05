cbca557e6091fd1c718cfbe8f59554f09830f521
guzmangj <guzmangj96@gmail.com>
2023-05-05 16:56:35 -0300

    View Admin, for de home limitado en 10

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,12 +4,12 @@ async function show(req, res) {}
 // Store a newly created resource in storage.
 async function store(req, res) {
   const comments = await Comment.create({
-      fullname: req.body.fullname,
-      content: req.body.content,
-      articleId: req.body.id
+    fullname: req.body.fullname,
+    content: req.body.content,
+    articleId: req.body.id,
   });
   console.log(res);
-  res.redirect(201, `/articulos/${comments.articleId}`)
+  res.redirect(`/articulos/${comments.articleId}`);
 }
 
 module.exports = {
