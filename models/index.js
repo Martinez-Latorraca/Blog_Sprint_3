23ac10952dfd7af47d684cb8ebda1e1ff037e50a
Victoria <mvperera@gmail.com>
2023-05-12 17:47:37 -0300

    models

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DB_DATABASE, // Ej: hack_academy_db
-  process.env.DB_USERNAME, // Ej: root
-  process.env.DB_PASSWORD, // Ej: root
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST, // Ej: 127.0.0.1
     dialect: process.env.DB_CONNECTION, // Ej: mysql
@@ -27,6 +27,9 @@ Article.hasMany(Comment);
 Article.belongsTo(User);
 Comment.belongsTo(Article);
 Comment.belongsTo(User);
+
+User.belongsTo(Role); // falta ordenarlos
+Role.hasMany(Users);
 
 module.exports = {
   sequelize,
